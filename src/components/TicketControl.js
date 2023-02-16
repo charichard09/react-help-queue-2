3f6c414859a7c50981fcb4cf08ca0153c16089b4
Richard <charichard09@gmail.com>
2023-02-15 16:17:36 -0800

    complete lesson on CRUD with hooks and NoSQL

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,18 +4,41 @@ import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
 import db from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 const TicketControl = () => {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(collection(db, "tickets"),
+    (collectionSnapshot) => {
+      // create an empty array to hold the tickets
+      const tickets = [];
+      // loop through collection object of returned ticket documents, for each document, push an object /w ticket properties into the tickets array
+      collectionSnapshot.forEach(doc => {
+        tickets.push({
+          // b/c doc.data() returns all of our documents data into an object, we can use the spread operator to spread all of the 
+          ...doc.data(),
+          id: doc.id
+        });
+      })
+      // set the mainTicketList state to the tickets array
+      setMainTicketList(tickets);
+    },
+    (error) => {
+      setError(error.message);
+    });
+
+    return () => unSubscribe();
+  }, []);
 
   const handleClick = () => {
     if (selectedTicket != null) {
       setFormVisibleOnPage(false);
-      // new code!
       setSelectedTicket(null);
       setEditing(false);
     } else {
@@ -23,25 +46,20 @@ const TicketControl = () => {
     }
   }
 
-  const handleDeletingTicket = (id) => {
-    const newMainTicketList = mainTicketList.filter(ticket => ticket.id !== id);
-    setMainTicketList(newMainTicketList);
-    // new code!
+  const handleDeletingTicket = async (id) => {
+    await deleteDoc(doc(db, "tickets", id));
     setSelectedTicket(null);
   }
 
   const handleEditClick = () => {
-    // new code!
     setEditing(true);
   }
 
-  const handleEditingTicketInList = (ticketToEdit) => {
-    const editedMainTicketList = mainTicketList
-    // new code: selectedTicket.id
-    .filter(ticket => ticket.id !== selectedTicket.id)
-    .concat(ticketToEdit);
-    setMainTicketList(editedMainTicketList);
-    // new code!
+  const handleEditingTicketInList = async (ticketToEdit) => {
+    // create a reference using doc() and the ticket id that is passed in
+    const ticketRef = doc(db, "tickets", ticketToEdit.id);
+    // await the updateDoc function that will use the ticketRef and update it in the database with the ticketToEdit object 
+    await updateDoc(ticketRef, ticketToEdit)
     setEditing(false);
     setSelectedTicket(null);
   }
@@ -63,7 +81,9 @@ const TicketControl = () => {
   let buttonText = null; 
 
 
-  if (editing) {      
+  if (error) {
+    currentlyVisibleState = <p>There was an error: {error}</p>
+  } else if (editing) {      
     currentlyVisibleState = 
       <EditTicketForm 
         ticket = {selectedTicket} 
@@ -92,7 +112,8 @@ const TicketControl = () => {
   return (
     <React.Fragment>
       {currentlyVisibleState}
-      <button onClick={handleClick}>{buttonText}</button> 
+      {/* ternary operator, if error true, return null, else return <button> */}
+      {error ? null : <button onClick={handleClick}>{buttonText}</button>} 
     </React.Fragment>
   );
 }
